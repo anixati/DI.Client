17481c2a5b0e7a08e54a7d43b639ae91d4ac28c8
ankornepati <ankornepati@XD401462>
2022-05-27 09:49:17 +1000

    Updated Data grid

--- apps/di-boards/src/app/pages/boards/appointee/appointeeList.tsx
+++ apps/di-boards/src/app/pages/boards/appointee/appointeeList.tsx
@@ -1,28 +1,31 @@
-import { PagedTable, PageView, ScrollPanel } from '@dotars/di-controls';
-import { IAuditLog, IAuditProp, IEntity } from '@dotars/di-core';
-import { ActionIcon, Grid, Group, Table, Text } from '@mantine/core';
-import moment from 'moment';
-import { useState } from 'react';
-import { CellProps, Column } from 'react-table';
-import { Eye, Receipt } from 'tabler-icons-react';
+
+
+
+import { PageView, SchemaListTable } from '@dotars/di-controls';
+import { Button, Group } from '@mantine/core';
+import { Receipt } from 'tabler-icons-react';
+import { appStyles } from '../../../Styles';
 
 export const AppointeeList: React.FC = () => {
+  const { classes } = appStyles();
   const createItem = () => {
     console.log('.');
   };
-  const viewItem = (row: IAuditLog) => {};
-
-  const columns: Array<Column<IEntity>> = [
-    {
-      Header: 'Id',
-      accessor: 'id',
-      width: 50,
-    }
-  ];
 
   return (
-    <PageView title="Audit Logs" desc="" icon={<Receipt />}>
-      <PagedTable<IEntity> title="Audit logs" OnCreate={createItem} columns={columns} canCreate={false} baseUrl="/audits" />
+    <PageView
+      title="Appointees"
+      desc=""
+      icon={<Receipt />}
+      renderCmds={() => {
+        return (
+          <Button variant="filled" color="dotars" className={classes.toolButton} onClick={() => createItem()}>
+          Create New
+        </Button>
+        );
+      }}
+    >
+      <SchemaListTable schemas={[{label:"Active Appointees",value:"ActiveAppointees"},{label:"Inactive Appointees",value:"InActiveAppointees"}]} />
     </PageView>
   );
 };
--- libs/di-controls/src/lib/data/tables/Datagrid.tsx
+++ libs/di-controls/src/lib/data/tables/Datagrid.tsx
@@ -1,10 +1,11 @@
-import { getErrorMsg, IDataResponse, IGenericListResponse, ISchemaDef, ITableDef } from '@dotars/di-core';
-import { Alert, Center, Group, LoadingOverlay, Table } from '@mantine/core';
+import { getErrorMsg, IColumnDef, IDataResponse, IEntity, IGenericListResponse, ISchemaDef, ITableDef } from '@dotars/di-core';
+import { ActionIcon, Alert, Anchor, Box, Center, Group, LoadingOverlay, NativeSelect, SelectItem, Table, TextInput } from '@mantine/core';
 import axios from 'axios';
-import { ReactElement, ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
+import { createContext, ReactElement, ReactNode, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
-import { usePagination, useSortBy, useTable } from 'react-table';
-import { ChevronDown, ChevronUp, Selector } from 'tabler-icons-react';
+import { Link } from 'react-router-dom';
+import { CellProps, Column, usePagination, useSortBy, useTable } from 'react-table';
+import { ChevronDown, ChevronUp, Eye, Refresh, Search, Selector } from 'tabler-icons-react';
 import { ScrollContent } from '../../panels';
 import { TableCmdBar } from '../controls';
 import { dataUiStyles } from '../Styles';
@@ -21,6 +22,19 @@ type SortInfo = {
   desc: boolean;
 };
 
+const LinkCol = (def: IColumnDef): Column<any> => {
+  return {
+    Header: `${def.Header}`,
+    id: `${def.accessor}-link`,
+    accessor:  `${def.accessor}`,
+    Cell: ({ row }: CellProps<any>) => (
+      <Anchor component={Link} to="/react-router" size="xs">
+        {row.original[def.accessor]}
+      </Anchor>
+    ),
+  };
+};
+
 function RenderDataGrid(rx: RenderTableProps): ReactElement {
   const { classes, cx } = dataUiStyles();
   const [pgSearch, setPgSearch] = useState('');
@@ -30,34 +44,49 @@ function RenderDataGrid(rx: RenderTableProps): ReactElement {
   const [pgCount, setPgCount] = useState<number>(0);
   const [pgSort, setPgSort] = useState<Array<any>>([]);
   const [items, setItems] = useState<any[]>([]);
+  const hiddenColumns: Array<string> = [];
   //const [{pgIndex, pgSize, pgSearch, pgSort}, SetState] = useState<TableState>(initialState);
   const QueryKey = `${rx.queryKey}`;
-  const fetchData = useCallback(async (index: number, size: number) => {
-    try {
-      setLoading(true);
-      console.log('loading ... ', index, size);
-      let sortBy: Array<SortInfo> = [];
-      if (pgSort.length > 0) {
-        sortBy = pgSort.map((x) => ({ id: x.id, desc: x.desc } as SortInfo));
+  const fetchData = useCallback(
+    async (index: number, size: number) => {
+      try {
+        setLoading(true);
+        console.log('loading ... ', index, size);
+        let sortBy: Array<SortInfo> = [];
+        if (pgSort.length > 0) {
+          sortBy = pgSort.map((x) => ({ id: x.id, desc: x.desc } as SortInfo));
+        }
+        const resp = await axios.post<IGenericListResponse<any>>(`/qry/schema/${QueryKey}`, { index, size, sortBy, SearchStr: pgSearch });
+        if (resp.data?.result) {
+          const rs = resp.data?.result;
+          setPgIndex(rs.pageIndex);
+          setPgSize(rs.pageSize);
+          setPgCount(rs.pageCount);
+          setItems(resp.data?.result?.items);
+        } else throw new Error(`Failed to retrive `);
+      } catch (ex) {
+        throw new Error(`API error:${getErrorMsg(ex)}`);
+      } finally {
+        setLoading(false);
       }
-      const resp = await axios.post<IGenericListResponse<any>>(`/qry/schema/${QueryKey}`, { index, size, sortBy,SearchStr:pgSearch });
-      if (resp.data?.result) {
-        const rs = resp.data?.result;
-        setPgIndex(rs.pageIndex);
-        setPgSize(rs.pageSize);
-        setPgCount(rs.pageCount);
-        setItems(resp.data?.result?.items);
-      } else throw new Error(`Failed to retrive `);
-    } catch (ex) {
-      throw new Error(`API error:${getErrorMsg(ex)}`);
-    } finally {
-      setLoading(false);
-    }
-  }, [QueryKey,pgSort,pgSearch]);
+    },
+    [QueryKey, pgSort, pgSearch]
+  );
 
   const memoizedColumns = useMemo(() => {
-    return rx.schema.columns.map((x) => ({ Header: x.Header, accessor: x.accessor, width: x.width }));
+    const colList = rx.schema.columns.map((x) => {
+      if (x.type === 1) hiddenColumns.push(x.accessor);
+      if (x.type === 2) {
+        hiddenColumns.push(x.accessor);
+        return LinkCol(x);
+      } else {
+        return { Header: x.Header, accessor: x.accessor, width: x.width };
+      }
+    });
+    console.log('--', colList);
+    return colList;
   }, [rx.schema]);
+
   const {
     getTableProps,
     getTableBodyProps,
@@ -74,16 +103,11 @@ function RenderDataGrid(rx: RenderTableProps): ReactElement {
     previousPage,
     setPageSize,
     state: { sortBy, pageIndex, pageSize },
-  } = useTable<any>({ columns: memoizedColumns, data: items, initialState: { pageIndex: pgIndex, pageSize: pgSize, sortBy: pgSort }, manualPagination: true, pageCount: pgCount }, useSortBy, usePagination);
+  } = useTable<any>({ columns: memoizedColumns, data: items, initialState: { pageIndex: pgIndex, pageSize: pgSize, sortBy: pgSort, hiddenColumns }, manualPagination: true, pageCount: pgCount }, useSortBy, usePagination);
 
   useEffect(() => {
     fetchData(pageIndex, pageSize);
   }, [fetchData, pageIndex, pageSize, pgSearch, pgSort]);
-
-  // useEffect(() => {
-  //   fetchData(pageIndex, pageSize, pgSearch, pgSort);
-  //   gotoPage(0);
-  // }, [fetchData, pageSize, gotoPage]);
 
   useEffect(() => {
     if (sortBy && sortBy.length > 0) {
@@ -91,24 +115,37 @@ function RenderDataGrid(rx: RenderTableProps): ReactElement {
     }
   }, [sortBy]);
 
-  // useEffect(() => {
-  //   if (pgSearch && pgSearch.length > 3) {
-  //     fetchData(pageIndex, pageSize);
-  //   }
-  // }, [pgSearch, fetchData]);
-
-  const OnSearch = (val: string) => {
-    if (val && val.length > 3) setPgSearch(val); else setPgSearch('')
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    if (value && value.length > 3) setPgSearch(value);
+    else setPgSearch('');
   };
-
   const OnRefresh = () => {
     fetchData(pageIndex, pageSize);
   };
-
+  const { schema, schemas, changeSchema } = useContext(SchemaContext);
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.currentTarget;
+    changeSchema?.(value);
+  };
+  //const [selValue, setSelValue] = useState('');
   return (
     <div className={classes.ptCard}>
       <div className={classes.ptheader}>
-        <TableCmdBar title={rx.schema.title}  OnSearch={OnSearch} OnRefresh={OnRefresh} renderCmds={rx.renderCmds} />
+        <Box>
+          <Group spacing="sm" position="apart">
+            <Group spacing="sm" position="left">
+              <NativeSelect data={schemas} variant="filled" value={schema} onChange={handleSelectChange} style={{ width: 310 }} />
+            </Group>
+            <Group position="right" spacing={3}>
+              <TextInput size="xs" placeholder="Search" icon={<Search size={14} color="#071E3E" />} onChange={handleSearchChange} />
+              <ActionIcon variant="filled" color="dotars" onClick={OnRefresh}>
+                <Refresh size={16} />
+              </ActionIcon>
+              {rx.renderCmds && rx.renderCmds()}
+            </Group>
+          </Group>
+        </Box>
       </div>
       <ScrollContent
         loading={loading}
@@ -182,5 +219,32 @@ export const SchemaTable: React.FC<SchemaTableProps> = (rx) => {
 
       {isSuccess && <RenderDataGrid queryKey={rx.schemaName} schema={data} renderCmds={rx.renderCmds} />}
     </>
+  );
+};
+//---------------------------------------------------------------------------------------------------------------
+
+interface ISchemaContext {
+  schema: string;
+  schemas: Array<SelectItem>;
+  changeSchema?: (name: string) => void;
+}
+
+const SchemaContext = createContext<ISchemaContext>({ schema: '', schemas: [] });
+
+export interface SchemaListTableProps {
+  schemas: Array<SelectItem>;
+  renderCmds?: () => ReactNode;
+}
+
+export const SchemaListTable: React.FC<SchemaListTableProps> = (rx) => {
+  const [schema, setSchema] = useState<string>(rx.schemas[0].value);
+  const [schemas] = useState<Array<SelectItem>>(rx.schemas);
+  const changeSchema = (name: string) => {
+    setSchema(name);
+  };
+  return (
+    <SchemaContext.Provider value={{ schema, schemas, changeSchema }}>
+      <SchemaTable schemaName={schema} />
+    </SchemaContext.Provider>
   );
 };
